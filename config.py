dd8b503e74e56e23c270ccf465660646b2701b69
Khurshidbek Kurbonboev <128981613+AnalyticKhurshid@users.noreply.github.com>
2025-08-03 12:18:29 +0500

    main

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = '7034361224:AAH97-mEWSZnqQsXSeuyvRGaQRjCB9PSyr8'  # Bot tokeningizni bu yerga qo'ying
+TOKEN = '7034361224:AAGM6AI-Y0lxzm12n4RQJLJW8Y-4UfRJZKQ'  # Bot tokeningizni bu yerga qo'ying
 ADMIN_ID = 1047421689  # Admin ID lar ro'yxati (o'zingizni Telegram ID'ingizni qo'ying)
 TIME_LIMIT = 20
